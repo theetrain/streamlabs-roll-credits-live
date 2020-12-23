1ef47eb74cd38ee125e5cb50bf546504fc0930c1
Enrico Sacchetti <enrico@theetrain.ca>
2020-12-23 17:57:15 -0500

    feat: prepare extension for testing

--- start.js
+++ start.js
@@ -1,25 +1,39 @@
 console.log('widget loaded')
 
-if (document.URL.includes('test=true')) {
-  console.log('Rolling credits')
-
-  const buttons = document.querySelectorAll('button')
-
-  for (let i = 0; i = buttons.length; i++ ) {
-    if (button.innerText.toLocaleLowerCase().trim() === 'test roll credits' ) {
-      button.click()
-      break;
+const rollCredits = () => {
+  if (document.URL.includes('/credits')) {
+    const buttons = document.querySelectorAll('button')
+    const clickButton = (matcher) => {
+      for (let i = 0; i < buttons.length; i++ ) {
+        if (buttons[i].innerText.toLocaleLowerCase().trim() === matcher ) {
+          buttons[i].click()
+          return true
+        }
+      }
     }
-  }
-} else if (document.URL.includes('activate=true')) {
-  console.log('Testing credits')
 
-  const buttons = document.querySelectorAll('button')
-
-  for (let i = 0; i = buttons.length; i++ ) {
-    if (button.innerText.toLocaleLowerCase().trim() === 'roll credits live' ) {
-      button.click()
-      break;
+    if (document.URL.includes('test=true')) {
+      console.log('Testing credits')
+      return clickButton('test roll credits')
+    } else if (document.URL.includes('activate=true')) {
+      console.log('Rolling credits live')
+      return clickButton('roll credits live')
     }
   }
 }
+
+// Make 5 attempts to click button
+let tries = 0
+
+const main = () => {
+  tries++
+  console.log(`Attempts to roll credits: ${tries}`)
+  if (rollCredits() !== true && tries <= 5) {
+    window.setTimeout(() => {
+      tries++
+      main()
+    }, 3000)
+  }
+}
+
+main()
